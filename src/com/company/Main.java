88c75ef6fd5581729eb0049307870fa72b35a272
Meagan Roberts <reaganmoberts@gmail.com>
2022-01-31 21:30:48 -0700

    change by Meagan

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is John.\n");
+        System.out.println("Hello John, my name is Meagan.");
     }
 }
